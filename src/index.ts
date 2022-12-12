c71a0e59c6594b0606b56f07014fff829afea194
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2022-12-11 21:19:43 -0300

    refactor: :coffin: removing useless code

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { LinkedList } from "./linked-list";
-
-const linkedList = new LinkedList();
-
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
-linkedList.append(5);
-
-linkedList.prepend(0);
-
-linkedList.insert(2, 10);
-
-linkedList.removeByIndex(2);
-
-console.log(linkedList.print());
+console.log("Take a look at the data structure folders");
